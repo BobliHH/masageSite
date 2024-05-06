32513ad9c8ccbd278a0a72a25eaf690b7a8d9ff7
AndreiBalasca <46415669+BobliHH@users.noreply.github.com>
2024-05-06 15:26:02 +0300

    done some refactoring

---
+++ index.js
@@ -0,0 +1,11 @@
+// Smooth scrolling to sections
+const links = document.querySelectorAll("nav a");
+
+links.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+    const target = document.querySelector(href);
+    target.scrollIntoView({ behavior: "smooth" });
+  });
+});
