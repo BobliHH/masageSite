f4bb019dfd308829174cb36aff37e335678b7d57
AndreiBalasca <46415669+BobliHH@users.noreply.github.com>
2024-05-08 15:09:57 +0300

    added more sections

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // Smooth scrolling to sections
 const links = document.querySelectorAll("nav a");
 
+
+
 links.forEach((link) => {
   link.addEventListener("click", (e) => {
     e.preventDefault();
