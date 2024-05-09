bc52ba1be03527d59fa7a5428bf67f7819c30d42
AndreiBalasca <46415669+BobliHH@users.noreply.github.com>
2024-05-09 15:35:55 +0300

    resolved svg's need to resolve clicks

--- index.js
+++ index.js
@@ -9,3 +9,14 @@ links.forEach((link) => {
     target.scrollIntoView({ behavior: "smooth" });
   });
 });
+
+const links1 = document.querySelectorAll("nav ms-3");
+
+links1.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+    const target = document.querySelector(href);
+    target.scrollIntoView({ behavior: "smooth" });
+  });
+});
